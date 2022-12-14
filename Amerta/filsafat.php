59aec12f9f03c4f72d37b23b4556b57690ea0fe1
Furqan-1A <takahirofurqan@gmail.com>
2022-12-14 17:47:47 +0700

    commit

--- Amerta/filsafat.php
+++ Amerta/filsafat.php
@@ -39,7 +39,6 @@
 include "proses/connect.php";
 $query  = mysqli_query($conn, "SELECT foto,judul,keterangan,kategori FROM tb_daftar_article WHERE id_daftar_article = '2'");
 while ($hasil = mysqli_fetch_array($query)) {
-
 ?>
     <div class="col-md-6">
         <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
@@ -50,33 +49,33 @@ while ($hasil = mysqli_fetch_array($query)) {
                 <a href="art1.php" class="stretched-link">Continue reading</a>
             </div>
             <div class="col-auto d-none d-lg-block">
-
                 <img src="assets/img/<?php echo $hasil["foto"]; ?>" width="200" height="250" alt="">
-
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-success">Design</strong>
-                <h3 class="mb-0">Post title</h3>
-                <div class="mb-1 text-muted">Nov 11</div>
-                <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="stretched-link">Continue reading</a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-                <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                </svg>
-
             </div>
         </div>
     </div>
 <?php
 }
 ?>
+
+
+<div class="col-md-6">
+    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-success">Design</strong>
+            <h3 class="mb-0">Post title</h3>
+            <div class="mb-1 text-muted">Nov 11</div>
+            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+            </svg>
+
+        </div>
+    </div>
+</div>
 <div class="container marketing ">
 
     <!-- Three columns of text below the carousel -->
